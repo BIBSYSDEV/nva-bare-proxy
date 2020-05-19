6553557c8663ef158a5121e3b24339367ae1799f
nr <nr@unit.no>
2020-05-19 12:16:26 +0200

    NP-911 Removed unused imports

--- src/test/java/no/unit/nva/bare/FetchAuthorityHandlerTest.java
+++ src/test/java/no/unit/nva/bare/FetchAuthorityHandlerTest.java
@@ -2,7 +2,6 @@ package no.unit.nva.bare;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,8 +20,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
