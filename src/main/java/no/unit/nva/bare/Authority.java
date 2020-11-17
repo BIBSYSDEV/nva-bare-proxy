9b15eec3e7f26ca982a040e5ccd895a82820a4b9
svenngjeto <svenn.gjeto@unit.no>
2020-11-17 08:51:21 +0100

    PMD fix, unused import

--- src/main/java/no/unit/nva/bare/Authority.java
+++ src/main/java/no/unit/nva/bare/Authority.java
@@ -1,7 +1,6 @@
 package no.unit.nva.bare;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.net.URI;
